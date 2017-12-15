a934927010c0cb3b84b18ad6a01f3fe6acc836ed
Rahul Prajapat <rahul.prajapat@proemion.com>
2017-12-15 22:21:42 +0100

    sample input data from gmaps directions api

---
+++ sample_input_from_directions_api.py
@@ -0,0 +1,23 @@
+import googlemaps
+from datetime import datetime
+import polyline
+
+gmaps = googlemaps.Client(key='AIzaSyDXHtLAgfmGCZxYiuHYnDjebooyU4_K4o4')
+
+'''
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+'''
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("db mindbox, berlin",
+                                     "berlin hbf",
+                                     mode="transit",
+                                     departure_time=now)
+
+print directions_result
